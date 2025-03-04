f105c28f7817398802fdf13f54a48a5626cbea11
MarkYnx <mark.yang@bitlayer.ltd>
2025-03-04 19:50:22 +0800

    complete the who transaction graph

--- src/transaction_graph.rs
+++ src/transaction_graph.rs
@@ -1,7 +1,10 @@
+use lazy_static::lazy_static;
+use std::clone;
 use std::str::FromStr;
 
 use bitcoin::script::Builder;
 use bitcoin::sighash::SighashCache;
+use bitcoin::taproot::{self, LeafVersion, TaprootBuilder, TaprootSpendInfo};
 use bitcoin::{
     key::{Keypair, Secp256k1},
     secp256k1::{All, Message, SecretKey},
@@ -11,8 +14,8 @@ use bitcoin::{
     opcodes::all::OP_NOP4, script::PushBytesBuf, transaction::Version, Amount, ScriptBuf,
     Transaction, TxOut,
 };
-use bitcoin::{OutPoint, TxIn, Txid, Witness};
-use secp256k1::rand;
+use bitcoin::{Network, OutPoint, TxIn, Txid, Witness};
+use secp256k1::{rand, SECP256K1};
 
 use crate::bitcoin_sdk::UTXO;
 use crate::{Error, TemplateHash};
@@ -33,13 +36,94 @@ impl Default for Params {
     }
 }
 
+lazy_static! {
+    pub static ref UNSPENDABLE_TAPROOT_PUBLIC_KEY: XOnlyPublicKey = XOnlyPublicKey::from_str(
+        "93c7378d96518a75448821c4f7c8f4bae7ce60f804d03d1f0628dd5dd0f5de51"
+    )
+    .unwrap();
+}
+
+#[derive(Clone)]
+pub struct TaprootInfo {
+    pub address: Address,
+    pub scripts: Vec<ScriptBuf>,
+    pub taproot_spend_info: TaprootSpendInfo,
+}
+
+pub fn build_taptree_with_script(
+    scripts: Vec<ScriptBuf>,
+    network: Network,
+) -> Result<TaprootInfo, Error> {
+    let n = scripts.len();
+    let taproot_builder = if n > 1 {
+        let m: u8 = ((n - 1).ilog2() + 1) as u8; // m = ceil(log(n))
+        let k = 2_usize.pow(m.into()) - n;
+        (0..n).fold(TaprootBuilder::new(), |acc, i| {
+            acc.add_leaf(m - ((i >= n - k) as u8), scripts[i].clone()).unwrap()
+        })
+    } else {
+        TaprootBuilder::new().add_leaf(0, scripts[0].clone()).unwrap()
+    };
+    let taproot_spend_info = taproot_builder
+        .finalize(SECP256K1, *UNSPENDABLE_TAPROOT_PUBLIC_KEY)
+        .expect("msg");
+
+    let secp: Secp256k1<All> = Secp256k1::new();
+    let address = Address::p2tr(
+        &secp,
+        *UNSPENDABLE_TAPROOT_PUBLIC_KEY,
+        taproot_spend_info.merkle_root(),
+        network,
+    );
+
+    Ok(TaprootInfo {
+        address: address.clone(),
+        scripts,
+        taproot_spend_info,
+    })
+}
+
+fn taproot_spend_info(
+    locking_scripts: Vec<ScriptBuf>,
+    index: usize,
+    internal_key: XOnlyPublicKey,
+) -> Result<TaprootSpendInfo, Error> {
+    let n = locking_scripts.len();
+    let m: u8 = ((n - 1).ilog2() + 1) as u8; // m = ceil(log(n))
+    let k = 2_usize.pow(m.into()) - n;
+    TaprootBuilder::new()
+        .add_leaf(m - ((index >= n - k) as u8), locking_scripts[index].clone())?
+        .finalize(SECP256K1, internal_key)
+        .map_err(|e|  {
+            println!("{:?}", e);
+            Error::UnknownError("Taproot not finalizable".into())})
+}
+
 pub struct TransactionGraph {
     kickoff: Transaction,
     happy_take: Transaction,
+
+    challenge: Transaction,
+    assert: Transaction,
+    disprove: Transaction,
+
+    taproot_info: TaprootInfo,
 }
 
 impl TransactionGraph {
-    pub fn dummy_input() -> TxIn {
+    fn dummy_utxo() -> UTXO {
+        UTXO {
+            outpoint: OutPoint {
+                txid: Txid::from_str(
+                    "defc8c2634291f74cf42dc16508b091d4a1ce1fb27f5a6861fe922e42a3c898b",
+                )
+                .expect(""),
+                vout: 0,
+            },
+            amount: Amount::ZERO,
+        }
+    }
+    fn dummy_input() -> TxIn {
         TxIn {
             previous_output: OutPoint {
                 txid: Txid::from_str(
@@ -55,21 +139,23 @@ impl TransactionGraph {
     }
 
     pub fn create_btc_tx(ins: &Vec<UTXO>, outs: Vec<(ScriptBuf, Amount)>) -> Transaction {
-        let input = ins.into_iter().map(|i| {
-            TxIn    {
-            previous_output: i.outpoint,
-            sequence: bitcoin::Sequence(0xFFFFFFFF),
-            script_sig: Builder::new().into_script(),
-            witness: Witness::new(),
-        }
-        }).collect();
+        let input = ins
+            .into_iter()
+            .map(|i| TxIn {
+                previous_output: i.outpoint,
+                sequence: bitcoin::Sequence(0xFFFFFFFF),
+                script_sig: Builder::new().into_script(),
+                witness: Witness::new(),
+            })
+            .collect();
 
-        let output = outs.into_iter().map(|o| {
-            TxOut {
+        let output = outs
+            .into_iter()
+            .map(|o| TxOut {
                 script_pubkey: o.0,
-                value: o.1
-            }
-        }).collect();
+                value: o.1,
+            })
+            .collect();
 
         Transaction {
             version: Version::TWO,
@@ -105,10 +191,57 @@ impl TransactionGraph {
         let happy_take_ctv_hash = happy_take.template_hash(0).expect("calc ctv hash");
         let lock_script_for_happy_take_input0 =
             Self::calc_locking_script(happy_take_ctv_hash).expect("calc lock script");
+
+        let disprove = {
+            let value = params.depoist_amt.to_sat() + params.stake_amt.to_sat()
+                - params.gas_amt.to_sat() * 5;
+            let ins = vec![TransactionGraph::dummy_utxo()];
+            let anchor_script_pubkey = ScriptBuf::from_hex("51024e73")
+                    .expect("statically valid script");
+            let outs = vec![(anchor_script_pubkey.clone(), Amount::from_sat(value/2)), (anchor_script_pubkey, Amount::from_sat(value-value/2))];
+            TransactionGraph::create_btc_tx(&ins, outs)
+        };
+
+        let assert = {
+            let value = params.depoist_amt.to_sat() + params.stake_amt.to_sat()
+                - params.gas_amt.to_sat() * 4;
+            let ins = vec![TransactionGraph::dummy_utxo()];
+            let disprove_ctv_hash = disprove.template_hash(0).expect("calc ctv hash");
+            let lock_script =
+                Self::calc_locking_script(disprove_ctv_hash).expect("calc lock script");
+            let outs = vec![(lock_script, Amount::from_sat(value))];
+            TransactionGraph::create_btc_tx(&ins, outs)
+        };
+
+        let challenge = {
+            let value = params.depoist_amt.to_sat() + params.stake_amt.to_sat()
+                - params.gas_amt.to_sat() * 3;
+            let ins = vec![TransactionGraph::dummy_utxo()];
+            let assert_ctv_hash = assert.template_hash(0).expect("calc ctv hash");
+            let lock_script = Self::calc_locking_script(assert_ctv_hash).expect("calc lock script");
+            let outs = vec![(lock_script, Amount::from_sat(value))];
+            TransactionGraph::create_btc_tx(&ins, outs)
+        };
+
+        let challenge_ctv_hash = challenge.template_hash(0).expect("calc ctv hash");
+        let lock_script_for_challenge_input0 =
+            Self::calc_locking_script(challenge_ctv_hash).expect("calc lock script");
+
+        let taproot_info = build_taptree_with_script(
+            vec![
+                lock_script_for_happy_take_input0.clone(),
+                lock_script_for_challenge_input0,
+            ],
+            Network::Regtest,
+        )
+        .expect("build success");
+
         let kickoff_output = TxOut {
             value: params.depoist_amt + params.stake_amt - params.gas_amt - params.gas_amt, // for pegin, kickoff
-            script_pubkey: lock_script_for_happy_take_input0,
+            script_pubkey: taproot_info.address.script_pubkey(),
+            // script_pubkey: lock_script_for_happy_take_input0
         };
+
         let kickoff = Transaction {
             version: Version::TWO,
             lock_time: bitcoin::locktime::absolute::LockTime::ZERO,
@@ -118,6 +251,11 @@ impl TransactionGraph {
         Self {
             kickoff,
             happy_take,
+
+            challenge,
+            assert,
+            disprove,
+            taproot_info
         }
     }
 
@@ -164,12 +302,71 @@ impl TransactionGraph {
         tx
     }
 
-    pub fn get_happy_take_tx(&self, kickoff_utxo: UTXO, signer: &SignerInfo) -> Transaction {
+    pub fn get_happy_take_tx(&self, kickoff_utxo: UTXO) -> Transaction {
         let mut tx = self.happy_take.clone();
         // replace input
         tx.input[0].previous_output = kickoff_utxo.outpoint;
 
+        let happy_take_script = self.taproot_info.scripts[0].clone();
+        let tsi = self.taproot_info.clone();
+        let cb = tsi 
+            .taproot_spend_info.control_block(&(happy_take_script.clone(), LeafVersion::TapScript))
+            .ok_or_else(|| Error::UnknownError("Taproot construction error".into())).expect("get control block");
+        tx.input[0].witness.push(happy_take_script);
+        tx.input[0].witness.push(cb.serialize());
+
         tx
+    }
+
+    pub fn get_challenge_tx(&self, kickoff_utxo: UTXO) -> Transaction {
+        let mut tx = self.challenge.clone();
+        // replace input
+        tx.input[0].previous_output = kickoff_utxo.outpoint;
+
+        let challenge_script = self.taproot_info.scripts[1].clone();
+        let tsi = self.taproot_info.clone();
+        let cb = tsi 
+            .taproot_spend_info.control_block(&(challenge_script.clone(), LeafVersion::TapScript))
+            .ok_or_else(|| Error::UnknownError("Taproot construction error".into())).expect("get control block");
+        tx.input[0].witness.push(challenge_script);
+        tx.input[0].witness.push(cb.serialize());
+
+        tx
+    }
+
+    pub fn get_assert_tx(&self, kickoff_utxo: UTXO) -> Transaction {
+        let mut tx = self.assert.clone();
+        // replace input
+        tx.input[0].previous_output = kickoff_utxo.outpoint;
+
+        tx
+    }
+
+    pub fn get_disprove_tx(&self, kickoff_utxo: UTXO, receiver: &SignerInfo) -> (Transaction, Transaction) {
+        let mut tx = self.disprove.clone();
+        // replace input
+        tx.input[0].previous_output = kickoff_utxo.outpoint;
+
+        let params = Params::default();
+        let amount = tx.output[0].value + tx.output[1].value - params.gas_amt;
+        let anchor_utxo = UTXO {
+            outpoint: OutPoint {
+                txid: tx.txid(),
+                vout: 0,
+            },
+            amount: tx.output[0].value
+        };
+        let anchor_utxo1 = UTXO {
+            outpoint: OutPoint {
+                txid: tx.txid(),
+                vout: 1,
+            },
+            amount: tx.output[1].value,
+        };
+        let ins = vec![anchor_utxo, anchor_utxo1];
+        let reward_tx = Self::create_btc_tx(&ins, vec![(receiver.address.script_pubkey(), amount)]);
+
+        (tx, reward_tx)
     }
 }
 
@@ -285,13 +482,17 @@ impl Pegin {
 }
 
 mod test {
-    use bitcoin::{transaction::{self, Version}, Amount, Network, OutPoint, Transaction, TxIn};
+    use bitcoin::{
+        transaction::{self, Version},
+        Amount, Network, OutPoint, Transaction, TxIn,
+    };
     use local_ip_address::local_ip;
 
     use crate::bitcoin_sdk::{RPCClient, UTXO};
 
     use super::{Params, Pegin, SignerInfo, TransactionGraph};
 
+    // pegin -> kickoff -> happytake
     #[test]
     fn test_transaction_graph_happy_path() {
         let params = Params::default();
@@ -334,9 +535,91 @@ mod test {
         };
 
         let happy_take_txid = client
-            .send_transaction(&transaction_graph.get_happy_take_tx(kickoff_utxo, &operator_siggner))
+            .send_transaction(&transaction_graph.get_happy_take_tx(kickoff_utxo))
             .expect("");
         println!("happy take txid: {}", happy_take_txid);
+    }
+
+    // pegin -> kickoff -> challenge -> assert -> disprove+reward
+    #[test]
+    fn test_transaction_graph_unhappy_path() {
+        let params = Params::default();
+        let operator_siggner = SignerInfo::new(bitcoin::Network::Regtest);
+        let transaction_graph = TransactionGraph::new(&operator_siggner.address, &params);
+        let kickoff_script = transaction_graph
+            .get_first_script()
+            .expect("get first script");
+
+        let url = format!("{}:18443", local_ip().expect("find one").to_string());
+        let user = "admin".to_string();
+        let password = "admin".to_string();
+
+        let client = RPCClient::new(&url, &user, &password);
+        let params = Params::default();
+        let utxo = client.prepare_utxo_for_address(params.depoist_amt, &operator_siggner.address);
+        let pegin = Pegin::new(utxo.outpoint, utxo.amount, kickoff_script, &params);
+        let pegin_tx = pegin.sign(&operator_siggner);
+        let pegin_txid = client.send_transaction(&pegin_tx).expect("success");
+        println!("pegin txid: {}", pegin_txid);
+        let pegin_utxo = UTXO {
+            outpoint: OutPoint {
+                txid: pegin_txid,
+                vout: 0,
+            },
+            amount: pegin_tx.output[0].value,
+        };
+        let stake_utxo =
+            client.prepare_utxo_for_address(params.stake_amt, &operator_siggner.address);
+        let kickoff_tx =
+            transaction_graph.get_kickoff_tx(pegin_utxo, stake_utxo, &operator_siggner);
+        let kickoff_txid = client.send_transaction(&kickoff_tx).expect("");
+        println!("kickoff txid: {}", kickoff_txid);
+        let kickoff_utxo = UTXO {
+            outpoint: OutPoint {
+                txid: kickoff_txid,
+                vout: 0,
+            },
+            amount: kickoff_tx.output[0].value,
+        };
+
+        let challenge_tx= transaction_graph.get_challenge_tx(kickoff_utxo);
+        let challenge_txid = client
+            .send_transaction(&challenge_tx)
+            .expect("");
+        println!("challenge txid: {}", challenge_txid);
+
+        let challenge_utxo = UTXO {
+            outpoint: OutPoint{
+                txid: challenge_txid,
+                vout: 0
+            },
+            amount: challenge_tx.output[0].value
+        };
+        let assert_tx= &transaction_graph.get_assert_tx(challenge_utxo);
+        let assert_txid = client
+        .send_transaction(&assert_tx)
+        .expect("");
+        println!("assert txid: {}", assert_txid);
+
+        let assert_utxo = UTXO {
+            outpoint: OutPoint{
+                txid: assert_txid,
+                vout: 0
+            },
+            amount: assert_tx.output[0].value
+        };
+
+        let receiver = SignerInfo::new(Network::Regtest);
+        let (disprove_tx, reward_tx)= transaction_graph.get_disprove_tx(assert_utxo, &receiver);
+        let disprove_txid = client
+        .send_transaction(&disprove_tx)
+        .expect("");
+        println!("disprove txid: {}", disprove_txid);
+
+        let reward_txid = client
+        .send_transaction(&reward_tx)
+        .expect("");
+        println!("reward txid: {}, amt: {}", reward_txid, reward_tx.output[0].value);
     }
 
     #[test]
@@ -372,12 +655,13 @@ mod test {
         //     transaction_graph.get_kickoff_tx(pegin_utxo, stake_utxo, &operator_siggner);
         let ins = vec![pegin_utxo, stake_utxo];
         let cheater_signer = SignerInfo::new(Network::Regtest);
-        let outs = vec![(cheater_signer.address.script_pubkey(), params.depoist_amt - params.gas_amt - params.gas_amt)];
-        let fake_kickoff = TransactionGraph::create_btc_tx(&ins, outs); 
+        let outs = vec![(
+            cheater_signer.address.script_pubkey(),
+            params.depoist_amt - params.gas_amt - params.gas_amt,
+        )];
+        let fake_kickoff = TransactionGraph::create_btc_tx(&ins, outs);
         let res = client.send_transaction(&fake_kickoff);
         println!("result: {:?}", res);
         assert_eq!(true, client.send_transaction(&fake_kickoff).is_err());
-    
-        
     }
 }
